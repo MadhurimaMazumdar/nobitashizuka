5a9d44252ed55c9ec3bcd7aa61f2864c3ccf9e25
MADHURIMA MAZUMDAR <1521022@kiit.ac.in>
2020-08-15 13:42:47 +0530

    hy

--- nobitashizuka/src/app/app-routing.module.ts
+++ nobitashizuka/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component.ts';
 
 
 const routes: Routes = [];
