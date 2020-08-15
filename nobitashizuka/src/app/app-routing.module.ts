3d7bb47681628141798883c83d7c0588bc4cb748
Bishal Biswas <--global>
2020-08-15 13:59:19 +0530

    router

--- nobitashizuka/src/app/app-routing.module.ts
+++ nobitashizuka/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component.ts';
+
+import { LandingComponent } from './landing/landing.component';
+import { HomeComponent } from './home/home.component';
+import { from } from 'rxjs';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',component: LandingComponent },
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
